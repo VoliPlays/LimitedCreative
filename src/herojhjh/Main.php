1031b610a9a6cd8255103b0cedea148fd21727b8
Herojhjh <mstivenpratama@outlook.com>
2017-06-28 13:06:08 +0700

    Update Main.php

--- src/herojhjh/Main.php
+++ src/herojhjh/Main.php
@@ -4,6 +4,7 @@ namespace herojhjh;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\utils\{TextFormat, Config};
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\item\ItemBlock;
 use pocketmine\permission\Permission;
@@ -12,9 +13,17 @@ use pocketmine\utils\TextFormat;
 class Main extends PluginBase implements Listener{
         public function onEnable(){
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
-                    $this->getLogger()->info("Enabled Plugin");
+            @mkdir($this->getDataFolder());
+            $this->saveDefaultConfig();
+            $this->reloadConfig();
+            $this->getLogger()->info("Enabled Plugin");
         }
    
         public function onInteract(PlayerInteractEvent $ev){
-        $IT = $this->getConfig()->get("ID"); 
-        if(($ev->getPlayer()->getGamemode() === 1) && ($ev->getBlock()->getId() === $IT)) $ev->setCancelled();
+         if($this->getConfig()->get("Limited.Creative") === true){
+          $IT = $this->getConfig()->get("id");
+          if(($ev->getPlayer()->getGamemode() === 1) && ($ev->getBlock()->getId() === $IT)) $ev->setCancelled();
+         }
+        }
+}
+     
